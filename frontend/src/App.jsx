86569501529c45360fccd24adebf8b671b80ee15
Nacho Palomeque <nachopalomeque5@gmail.com>
2026-09-14 13:29:54 -0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import ChatView from './pages/ChatView'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/chat">Chat Interface</Link>
+      </nav>
+
+      {/* The Routes block dictates which page loads based on the URL */}
+      <div style={{ padding: '2rem' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatView />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
---
+++ frontend/src/pages/ChatView.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+
+export default function ChatView() {
+  return (
+    <div>
+        <h1>Chat View</h1>
+        <Link to="/">Go to gome page</Link>
+    </div>
+  )
+}
